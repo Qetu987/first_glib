3b6a8948d31df81b3d8c5ffafed2b5154b0c0912
Ruslan <sichkaryk39@gmail.com>
2026-04-06 20:31:24 +0300

    Implement IndexPage and SubPage views, update templates for new structure and styling

--- client/views.py
+++ client/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
-class BasePage(View):
+class IndexPage(View):
     
     def get(self, request):
         context = {'name': 'John'}
 
-        return render(request, 'base.html', context)
+        return render(request, 'test_pages/index.html', context)
+
+
+class SubPagePage(View):
+    
+    def get(self, request):
+        context = {'name': 'Bob'}
+
+        return render(request, 'test_pages/sub_page.html', context)
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from client.views import BasePage
+from client.views import IndexPage, SubPagePage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', BasePage.as_view(), name='base_page')
+    path('', IndexPage.as_view(), name='index_page'),
+    path('index/', SubPagePage.as_view(), name='sub_page_page'),
 ]
